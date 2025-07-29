5bedb270a0ec7df433b2e98654cc873718d7b2ef
Jaymin Makwana <jaymin.makwana@tatvasoft.com>
2025-07-29 17:53:45 +0530

    fixed

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,11 +9,11 @@ echo "Logging into Amazon ECR..."
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $REPO
 
 echo "Stopping and removing any existing container..."
-sudo docker stop my-app || true
-sudo docker rm my-app || true
+docker stop my-app || true
+docker rm my-app || true
 
 echo "Pulling latest image..."
-sudo docker pull $REPO:latest
+docker pull $REPO:latest
 
 echo "Running container..."
-sudo docker run -d --name my-app -p 3000:3000 $REPO:latest
+docker run -d --name my-app -p 3000:3000 $REPO:latest
