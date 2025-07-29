8550c03008d5d4b7b7233e68361b429e122e5c10
Jaymin Makwana <jaymin.makwana@tatvasoft.com>
2025-07-29 15:51:59 +0530

    updated

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,9 +8,9 @@ REPO="127145910156.dkr.ecr.ap-south-1.amazonaws.com/docker/nodejs"
 echo "Logging into Amazon ECR..."
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $REPO
 
-# echo "Stopping and removing any existing container..."
-# sudo docker stop my-app || true
-# sudo docker rm my-app || true
+echo "Stopping and removing any existing container..."
+sudo docker stop my-app || true
+sudo docker rm my-app || true
 
 echo "Pulling latest image..."
 sudo docker pull $REPO:latest
