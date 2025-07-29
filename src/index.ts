705185ad04ba9f74e36eb4edb8262e92bd7f4c51
Jaymin Makwana <jaymin.makwana@tatvasoft.com>
2025-07-29 18:03:18 +0530

    chnage file

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (_req, res) => {
-  res.send('Hello from TypeScript + Express from docker + CI/CD + ECS updated V2 Updated!');
+  res.send('Hello from TypeScript + Express from docker + CI/CD + ECS updated V2 Updated EC2!');
 });
 
 // Node.js example
