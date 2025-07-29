bcb181725dbf699d02764c27760751139bf23373
Jaymin Makwana <jaymin.makwana@tatvasoft.com>
2025-07-29 12:16:19 +0530

    add appspec file

---
+++ scripts/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+REGION="ap-south-1"
+REPO="127145910156.dkr.ecr.ap-south-1.amazonaws.com/docker/nodejs"
+
+echo "Logging into Amazon ECR..."
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $REPO
+
+echo "Stopping and removing any existing container..."
+docker stop my-app || true
+docker rm my-app || true
+
+echo "Pulling latest image..."
+docker pull $REPO:latest
+
+echo "Running container..."
+docker run -d --name my-app -p 3000:3000 $REPO:latest
